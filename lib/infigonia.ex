9e5618a04c61fb954f448f8c808d421265ec016e
Junaid Farooq <junaid@evercam.io>
2022-05-18 22:28:39 +0500

    Added Rates Poller definition.

--- lib/infigonia.ex
+++ lib/infigonia.ex
@@ -1,18 +1,2 @@
 defmodule Infigonia do
-  @moduledoc """
-  Documentation for `Infigonia`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Infigonia.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
---
+++ lib/infigonia/usd_conversion_rates/poller.ex
@@ -0,0 +1,32 @@
+defmodule Infigonia.UsdConversionRates.Poller do
+  use GenServer
+
+  require Logger
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @spec init(map()) :: {:ok, any, {:continue, :fetch_rates}}
+  def init(state) do
+    {:ok, state, {:continue, :fetch_rates}}
+  end
+
+  @spec handle_continue(:fetch_rates, map) ::
+          {:noreply, %{:clock => reference, optional(any) => any}}
+  def handle_continue(:fetch_rates, state) do
+    Logger.info("Fetching the rates and setting the clock")
+
+    clock = Process.send_after(self(), :fetch_rates, 10_000)
+
+    {:noreply, Map.put(state, :clock, clock)}
+  end
+
+  def handle_info(:fetch_rates, state) do
+    Logger.info("Fetching the rates and setting the clock from ticker")
+
+    clock = Process.send_after(self(), :fetch_rates, 10_000)
+
+    {:noreply, Map.put(state, :clock, clock)}
+  end
+end
