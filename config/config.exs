b344ce5f875596f660b4dea549f54e64bd76d336
Junaid Farooq <junaid@evercam.io>
2022-05-18 21:45:35 +0500

    Added api details for exchangerates API.

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,8 @@ import Config
 config :infigonia,
   ecto_repos: [Infigonia.Repo]
 
+config :infigonia,
+  exchangerates_api_key: "Ox1lQeV8KiH7P79hp7I9dGpDJQeUcFx3",
+  exchangerates_api: "https://api.apilayer.com/exchangerates_data/latest"
+
 import_config "#{config_env()}.exs"
